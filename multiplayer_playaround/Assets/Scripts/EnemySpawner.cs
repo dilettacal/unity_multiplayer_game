cf63f15f60e84e19a5701bc7ee4287fa6880cfcd
Di Cal <diletta.cal@gmail.com>
2018-07-21 19:18:48 +0200

    Enemies from server

---
+++ multiplayer_playaround/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class EnemySpawner : NetworkBehaviour
+{
+
+    public GameObject enemyPrefab;
+    public int numberOfEnemies;
+
+    //VIrtual Function
+    //Server starts listening to the Network
+    public override void OnStartServer() //similar to OnStartLocalPlayer
+    {
+        //When server starts --> random number of enemies at a random position and rotation
+        //Spawned through NetworkServer.Spawn(enemy)
+        for (int i = 0; i < numberOfEnemies; i++)
+        {
+            var spawnPosition = new Vector3(
+                Random.Range(-8.0f, 8.0f),
+                0.0f,
+                Random.Range(-8.0f, 8.0f));
+
+            var spawnRotation = Quaternion.Euler(
+                0.0f,
+                Random.Range(0, 180),
+                0.0f);
+
+            var enemy = (GameObject)Instantiate(enemyPrefab, spawnPosition, spawnRotation);
+            NetworkServer.Spawn(enemy);
+        }
+    }
+}
