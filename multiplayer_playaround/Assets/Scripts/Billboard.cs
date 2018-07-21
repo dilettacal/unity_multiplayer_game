99f6384f0111dcd040121e77f215607034acbac3
Di Cal <diletta.cal@gmail.com>
2018-07-21 16:33:25 +0200

    Bugs removed

--- multiplayer_playaround/Assets/Scripts/Billboard.cs
+++ multiplayer_playaround/Assets/Scripts/Billboard.cs
@@ -1,16 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class Billboard : MonoBehaviour {
+public class Billboard : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Update()
+    {
+        transform.LookAt(Camera.main.transform);
+    }
 }
